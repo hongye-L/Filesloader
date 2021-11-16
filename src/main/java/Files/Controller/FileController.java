4f75c61cb06f58e001a601705ed8029f7204fc5d
lucky-lxb <13925980658@163.com>
2021-11-16 13:05:30 +0800

    文件上下传接口

---
+++ src/main/java/Files/Controller/FileController.java
@@ -0,0 +1,70 @@
+package Files.Controller;
+import Files.Model.Files;
+import Files.Service.FileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+@RestController
+public class FileController {
+    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+
+    @Autowired
+    private FileService fileService;
+
+    @PostMapping("/uploadFile")
+    public Files uploadFile(@RequestParam("file") MultipartFile file){
+        String fileName = fileService.storeFile(file);
+
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/downloadFile/")
+                .path(fileName)
+                .toUriString();
+
+        return new Files(fileName, fileDownloadUri,
+                file.getContentType(), file.getSize());
+    }
+
+    @PostMapping("/uploadMultipleFiles")
+    public List<Files> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        List<Files> list = new ArrayList<>();
+        if (files != null) {
+            for (MultipartFile multipartFile:files) {
+                Files uploadFileResponse = uploadFile(multipartFile);
+                list.add(uploadFileResponse);
+            }
+        }
+        return list;
+    }
+
+    @GetMapping("/downloadFile/{fileName:.*}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+        Resource resource = fileService.loadFileAsResource(fileName);
+        String contentType = null;
+        try {
+            request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException e) {
+            logger.info("Could not determine file type.");
+        }
+        if(contentType == null) {
+            contentType = "application/octet-stream";
+        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+
+}
---
+++ src/main/java/Files/SpringbootUploadApplication.java
@@ -0,0 +1,15 @@
+package Files;
+
+import Files.util.FileProperty;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+@SpringBootApplication
+@EnableConfigurationProperties({
+        FileProperty.class
+})
+public class SpringbootUploadApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootUploadApplication.class, args);
+    }
+}
